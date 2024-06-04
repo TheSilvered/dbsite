6850ecf4e8f760c2a22ba3378ac54c53cde12f83
TheSilvered <taffarello.da@gmail.com>
2024-06-04 22:13:36 +0200

    Removed venv and pycache

--- Activate.ps1
+++ Activate.ps1
@@ -1,1 +1,19 @@
+if (!(Test-Path -Path venv\))
+{
+    python -m venv venv
+}
+
 venv\Scripts\Activate.ps1
+
+$packages = python -m pip list
+
+if (!($packages -like "*django*"))
+{
+    python -m pip install django
+}
+
+if (!($packages -like "*matplotlib*"))
+{
+    python -m pip install django
+}
+
--- dbsite/albatramonto/views.py
+++ dbsite/albatramonto/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from .models import OreLuce
 
 def albatramonto_home(request):
-    dati = OreLuce.objects.raw("SELECT * FROM albatramonto_oreluce")
+    if request.GET and "datainizio" in request.GET:
+        dati = OreLuce.objects.raw(f'SELECT * FROM albatramonto_oreluce WHERE data >= "{request.GET["datainizio"]}"')
+    else:
+        dati = OreLuce.objects.raw('SELECT * FROM albatramonto_oreluce')
     return render(request, 'albatramonto_home.html', {"dati": dati})
